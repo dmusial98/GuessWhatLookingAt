3c6a8db2f36cfe8cc0b0095639da7ea54a6f2f04
dmusial98 <dmusial98@gmail.com>
2020-11-25 16:26:19 +0100

    first correct use msgpack

--- zeroMQ/PupilRequestClient/Program.cs
+++ zeroMQ/PupilRequestClient/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NetMQ.Sockets;
 using NetMQ;
-
+using SimpleMsgPack;
 using System.IO;
 
 namespace PupilRequestClient
@@ -18,6 +18,8 @@ namespace PupilRequestClient
             {
                 client.Connect("tcp://127.0.0.1:50020");
 
+
+                //getting subscriber and publisher port
                 client.SendFrame("SUB_PORT");
                 var subPort = client.ReceiveFrameString();
                 Console.WriteLine("SUB_PORT: {0}", subPort);
@@ -28,46 +30,88 @@ namespace PupilRequestClient
 
                 using (var subscriber = new SubscriberSocket())
                 {
-                    subscriber.Connect("tcp://127.0.0.1:" + subPort );
+                    //connect to zmq subscriber port and getting gaze data
+                    subscriber.Connect("tcp://127.0.0.1:" + subPort);
                     subscriber.Subscribe("gaze.");
 
-                   
-                        try
-                        {
-                            //Pass the filepath and filename to the StreamWriter Constructor
-                            StreamWriter sw = new StreamWriter("C:\\Users\\dmusi\\source\\repos\\zeroMQ\\PupilRequestClient\\Gaze_Pupil.txt");
+                    //decimal[] norm_pos = { 0.1295810100458894m, 0.3207013110574589m };
+                    //var ellipse = new PupilGaze3d.Ellipse
+                    //{
+                    //    angle = -18.24879778134816m
+                    //};
 
-                        for (int i = 0; i < 5; i++) 
-                        {
-                            var gaze = subscriber.ReceiveFrameBytes();
-                            var msg = subscriber.ReceiveFrameString();
+                    //var serializeContent = new PupilGaze3d
+                    //{
+                    //    id = 0,
+                    //    topic = "pupil.0.3d",
+                    //    method = "3d c++",
+                    //    norm_pos = norm_pos,
+                    //    diameter = 52.36177621161693m,
+                    //    timestamp = 114229.990821m,
+                    //    confidence = 0.9989116277448433m,
+                    //    ellipse = ellipse,
+                    //};
 
-                            sw.WriteLine("Data length: {0}", gaze.Length);
-                            Console.WriteLine("Data length: {0}", gaze.Length);
-                            foreach (var element in gaze)
-                            {
-                                sw.Write("{0} ", element.ToString("X"));
-                                Console.WriteLine("0x{0} ", element.ToString("X"));
-                            }
-                            
-                            sw.WriteLine();
-                            Console.ReadKey();
-                            }
+                    try
+                    {
+                        //open file for saving data
+                        StreamWriter sw = new StreamWriter("C:\\Users\\dmusi\\source\\repos\\GuessWhatLookingAt\\zeroMQ\\PupilRequestClient\\Gaze_Pupil.txt");
 
-                            //Close the file
-                            sw.Close();
-                        }
-                        catch (Exception e)
+                        //for (int i = 0; i < 10; i++)
+                        //{
+                        var msg = subscriber.ReceiveFrameString();
+                        var gaze = subscriber.ReceiveFrameBytes();
+
+                        sw.WriteLine("Data length: {0}", gaze.Length);
+                        sw.WriteLine("Text: {0}", msg);
+
+                        Console.WriteLine("Data length: {0}", gaze.Length);
+                        Console.WriteLine("Text: {0}", msg);
+
+                        foreach (var element in gaze)
                         {
-                            Console.WriteLine("Exception: " + e.Message);
+                            sw.Write("{0} ", element.ToString("X"));
+                            Console.WriteLine("0x{0} ", element.ToString("X"));
                         }
-                        finally 
-                        {
-                            Console.WriteLine("Executing finally block.");
-                        }
+
+                        sw.WriteLine();
+
+                        MsgPack unpackMsgPack = new MsgPack();
+                        unpackMsgPack.DecodeFromBytes(gaze);
+
+                        var baseData = unpackMsgPack.ForcePathObject("base_data").AsArray;
+
+                        Console.WriteLine("method: {0}", unpackMsgPack.ForcePathObject("base_data").AsArray[0].ForcePathObject("method").AsString);
+                        Console.WriteLine("topic: {0}", unpackMsgPack.ForcePathObject("topic").AsString);
+                        //Console.WriteLine("method: {0}", unpackMsgPack.ForcePathObject("method").AsString);
+                        Console.WriteLine("confidence: {0}", unpackMsgPack.ForcePathObject("confidence").AsFloat);
+                        Console.WriteLine("phi: {0}", unpackMsgPack.ForcePathObject("base_data").AsArray[0].ForcePathObject("phi").AsFloat);
+                        Console.WriteLine("theta: {0}", unpackMsgPack.ForcePathObject("base_data").AsArray[0].ForcePathObject("theta").AsFloat);
+
+                        sw.WriteLine("method: {0}", unpackMsgPack.ForcePathObject("base_data").AsArray[0].ForcePathObject("method").AsString);
+                        sw.WriteLine("topic: {0}", unpackMsgPack.ForcePathObject("topic").AsString);
+                        //Console.WriteLine("method: {0}", unpackMsgPack.ForcePathObject("method").AsString);
+                        sw.WriteLine("confidence: {0}", unpackMsgPack.ForcePathObject("confidence").AsFloat);
+                        sw.WriteLine("phi: {0}", unpackMsgPack.ForcePathObject("base_data").AsArray[0].ForcePathObject("phi").AsFloat);
+                        sw.WriteLine("theta: {0}", unpackMsgPack.ForcePathObject("base_data").AsArray[0].ForcePathObject("theta").AsFloat);
+
+                        Console.ReadKey();
+                        //}
+
+                        //Close the file
+                        sw.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Executing finally block.");
+                    }
 
                     Console.ReadKey();
-                }   
+                }
             }
         }
     }
